fa91c829be0c8bdb9f2b330e06906df349471b20
Jason Zhang <jason.gp.zhang@gmail.com>
2025-01-15 23:33:53 +0800

    add project files
    Signed-off-by: Jason Zhang <jason.gp.zhang@gmail.com>

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Tumbleweed
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
