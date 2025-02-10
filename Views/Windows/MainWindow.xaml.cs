75601fd021f5c08502006c8b02e030f8f3f7afe6
Jason Zhang <jason.gp.zhang@gmail.com>
2025-02-10 16:54:20 +0800

    Views: Windows: MainWindow: navigate to home page when window is loaded
    Signed-off-by: Jason Zhang <jason.gp.zhang@gmail.com>

--- Views/Windows/MainWindow.xaml.cs
+++ Views/Windows/MainWindow.xaml.cs
@@ -1,10 +1,22 @@
-﻿namespace Tumbleweed.Views.Windows
+﻿using System.Windows;
+using Tumbleweed.Views.Pages;
+
+namespace Tumbleweed.Views.Windows
 {
     public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            if (sender is MainWindow mainWindow)
+            {
+                mainWindow.NavigationView.Navigate(typeof(HomePage));
+            }
         }
     }
 }
