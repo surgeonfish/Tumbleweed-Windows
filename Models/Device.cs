20fe18f1c28b8240342aca847d1b26860d3a00f7
Jason Zhang <jason.gp.zhang@gmail.com>
2025-02-12 23:38:00 +0800

    Models: add device
    This patch add 3 properties for a device:
     - Name: It could be "Desktop-xxx", "Google Pixel 5",
       "iPhone 12" or something else.
     - Type: "PC" or "Phone".
     - IPaddress: IP address of the device, it can be IPv4
       and IPv6.
    
    Signed-off-by: Jason Zhang <jason.gp.zhang@gmail.com>

---
+++ Models/Device.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Sockets;
+
+namespace Tumbleweed.Models
+{
+    class Device
+    {
+        public string Name { set; get; }
+
+        public string Type { set; get; }
+
+        public string IPAddress { set; get; }
+
+        private readonly TcpClient _tcpClient;
+
+        public Device(TcpClient tcpClient)
+        {
+            _tcpClient = tcpClient;
+        }
+    }
+}
